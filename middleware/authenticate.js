2afef299c09ade74082f1bb2e17d4ca10b63c85a
Aditya Dubey <dubeyaditya6232@gmail.com>
2022-10-13 20:13:59 +0530

    added jwt middleware

---
+++ middleware/authenticate.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+exports.verifyUser =(req,res,next) =>{
+    const bearer =req.headers.authorization;
+    if(bearer){
+        const token = bearer.slice(7);
+        console.log(token);
+        jwt.verify(token,"secretKey", (err,decoded) =>{
+            if(err){
+                res.status(401).json({message:"User not authenticated!"});
+            }
+            else{
+            req.userData=decoded;
+                next();
+            }
+        });
+    }else{
+        res.status(401).json({message:"Login token not found!"});
+    }
+}
